7891b9d6ba7d337a4fc999faf3f1c3c400593c25
Neo-15 <71397872+Neo-15@users.noreply.github.com>
2020-11-05 17:06:40 +0200

    files

--- files/java/apps.js
+++ files/java/apps.js
@@ -1,72 +1,66 @@
 $(function(){
-var header=$("#header"),
-introH = $("#intro").innerHeight(),
-scrollOffset =$(window).scrollTop();
+ var header=$("#header"),
+ introH = $("#intro").innerHeight(),
+ scrollOffset =$(window).scrollTop();
 
-/* Fixed Header */	
-checkScroll(scrollOffset);
+ /* Fixed Header */	
+ checkScroll(scrollOffset);
 	
 	
-$(window).on("scroll", function(){
-scrollOffset = $(this).scrollTop();
-checkScroll(scrollOffset);
-});
+ $(window).on("scroll", function(){
+   scrollOffset = $(this).scrollTop();
+   checkScroll(scrollOffset);
+ });
 
-	function checkScroll(scrollOffset){
-if(scrollOffset>=introH){
-header.addClass("header--fixed");
-} else{
-header.removeClass("header--fixed");
-}
-	}
+ function checkScroll(scrollOffset){
+   if(scrollOffset>=introH){ 
+   header.addClass("header--fixed");
+   } 
+   else{
+     header.removeClass("header--fixed");
+   }
+  }
 	
-/* Smooth scroll */
-$("[data-scroll]").on("click", function(event) {
-event.preventDefault();
-var blockid = $(this).data("scroll"),
-blockOffset = $(blockid).offset().top;
+ /* Smooth scroll */
+ $("[data-scroll]").on("click", function(event) {
+   event.preventDefault();
+   var blockid = $(this).data("scroll"),
+   blockOffset = $(blockid).offset().top;
 
-$("html, body").animate({
-scrollTop: blockOffset
-},500);
+ $("html, body").animate({
+   scrollTop: blockOffset},500);
+ });
 	
-});
-	
-/* menu nav togle */
-$("#nav-togle ").on("click", function(event){
-event.preventDefault();
+ /* menu nav togle */
+ $("#nav-togle ").on("click", function(event){
+   event.preventDefault();
+   $("#nav").toggleClass("active");
+   $("#header").toggleClass("active");	
+ });
 
-	$("#nav").toggleClass("active");
-	$("#header").toggleClass("active");
-	
-});
-
-/*slider*/
-	$("[data-slider]").slick({
-slidesToShow: 2,  
-  slidesToScroll: 2,
-  responsive: [
+ /*slider*/
+ $("[data-slider]").slick({
+   slidesToShow: 2,  
+   slidesToScroll: 2,
+   responsive: [
     {
       breakpoint: 500,
-      settings: {
-       slidesToShow: 1,
-       slidesToScroll: 1,
-		arrows:false,
-		dots:true,
-		 fade:true,
-		 infinite:true,
-		 draggable:true,
-		 swipe:true
+      	settings: {
+      	slidesToShow: 1,
+      	slidesToScroll: 1,
+	  	arrows:false,
+	  	dots:true,
+	  	fade:true,
+	  	infinite:true,
+	  	draggable:true,
+	  	swipe:true
+      }
      }
-    }
-  	]     
-
-	
-	
-})	
+   ]     
+ })	
 });
 
 $('.intro, .block-about, .work, .our-work, .reviev, .clients, .contacts, .footer, footer').click(function() {
     $('.nav').removeClass('active');
 	$("#header").removeClass("active");
-  });
+ });
